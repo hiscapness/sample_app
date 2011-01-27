300b9018d564126f12fdb002614f8df356f5d032
U-cappy-lt\cappy <cappy@cappy-lt.(none)>
2011-01-26 20:26:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9237208488c242e84d5c517017d25a214a489cd920d3133c7612fc65390572fd196b14597a77c077ed02db9372ab2e9a79d66b6dc18dad203ed45b89a6edfe40'
